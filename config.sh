2211d12892967ed7f564f9234b272f786ce56b8a
root <root@localhost.localdomain>
2018-04-11 16:35:18 -0400

    fix gef sourcing in gdbinit

--- config.sh
+++ config.sh
@@ -5,10 +5,9 @@ apt install tmux -y
 find / -type f -iname *gnome.terminal.desktop -exec sed -i -e "s/^Exec=gnome-terminal/Exec=gnome-terminal -e \"tmux -2\"/" {} \;
 cp ./.tmux.conf ~
 echo "[*] Tmux configured"
-cp ./.gdbinit ~
-sleep 1
+echo "[*] Configuring vim"
 cp ./.vimrc ~
-echo "[*] Config files copied to their places"
+echo "[*] Vim setup complete"
 echo "[*] Installing GoLang"
 apt install golang -y
 echo "export GOPATH=~/go" > ~/.bashrc
@@ -17,5 +16,8 @@ echo "[*] Setting up bash environment"
 gsettings set org.gnome.shell favorite-apps "['firefox-esr.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'kali-burpsuite.desktop']"
 echo 'export PS1="[\[$(tput sgr0)\]\[\033[38;5;14m\]\t\[$(tput sgr0)\]\[\033[38;5;15m\] \u@\h:\[$(tput sgr0)\]\[\033[38;5;10m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]]\n\\$ \[$(tput sgr0)\]"' > ~/.bashrc
 wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
-echo "source ~.gdbinit-gef.py" >> ~/.gdbinit
+echo "[*] Configuring gdb and gef"
+cp ./.gdbinit ~
+echo "source ~/.gdbinit-gef.py" >> ~/.gdbinit
+echo "[*] gdb configuration complete"
 echo "[*] Environment setup complete"
