5c25795b3a292ec696230f90a411913d7dea4a95
root <root@localhost.localdomain>
2018-04-11 16:53:12 -0400

    add gef python3 re-reqs

--- config.sh
+++ config.sh
@@ -15,9 +15,14 @@ echo "[*] GoLang installed and configured"
 echo "[*] Setting up bash environment"
 gsettings set org.gnome.shell favorite-apps "['firefox-esr.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'kali-burpsuite.desktop']"
 echo 'export PS1="[\[$(tput sgr0)\]\[\033[38;5;14m\]\t\[$(tput sgr0)\]\[\033[38;5;15m\] \u@\h:\[$(tput sgr0)\]\[\033[38;5;10m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]]\n\\$ \[$(tput sgr0)\]"' > ~/.bashrc
-wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
 echo "[*] Configuring gdb and gef"
 cp ./.gdbinit ~
+echo "[*] Installing python re-reqs"
+pip3 install keystone-engine
+pip3 install unicorn
+pip3 install capstone
+pip3 install ropper
+wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
 echo "source ~/.gdbinit-gef.py" >> ~/.gdbinit
 echo "[*] gdb configuration complete"
 echo "[*] Environment setup complete"
