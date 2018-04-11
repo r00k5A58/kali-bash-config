94981831cb65547f8edf81d71e6da4c02861cd89
root <root@localhost.localdomain>
2018-04-11 15:38:14 -0400

    terminating search string in sed statement

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ echo "export GOPATH=~/go" > ~/.bashrc
 echo "[*] GoLang installed and configured"
 echo "[*] Setting up bash environment"
 echo 'export PS1="[\[$(tput sgr0)\]\[\033[38;5;14m\]\t\[$(tput sgr0)\]\[\033[38;5;15m\] \u@\h:\[$(tput sgr0)\]\[\033[38;5;10m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]]\n\\$ \[$(tput sgr0)\]"' > ~/.bashrc
-find / -type f -iname *gnome.terminal.desktop -exec sed -i -e "s/Exec=gnome-terminal/Exec=gnome-terminal -e \"tmux -2\"" {} \;
+find / -type f -iname *gnome.terminal.desktop -exec sed -i -e "s/Exec=gnome-terminal/Exec=gnome-terminal -e \"tmux -2\"/" {} \;
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 echo "[*] Environment setup complete"
